0667010ae5e7790216fb211b27a142db6acffad1
Slighthand <adeline.siu251@ycdsbk12.ca>
2025-03-21 19:04:41 -0400

    Initial commit

---
+++ Assets/Scripts/CoinCollision.cs
@@ -0,0 +1,25 @@
+using UnityEngine;
+
+public class CoinCollsion : MonoBehaviour
+{
+    public CoinManager cm;
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        void OnTriggerEnter2D(Collider2D other)
+        {
+            if (other.gameObject.CompareTag("Coin"))
+            {
+                Destroy(other.gameObject);
+                cm.CoinCount++;
+
+            }
+        }
+    }
+}
---
+++ Assets/Scripts/CoinManager.cs
@@ -0,0 +1,18 @@
+using UnityEngine;
+
+public class CoinManager : MonoBehaviour
+{
+    public int CoinCount;
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        // make coin count
+        // make only 1 coin appear at a time
+    }
+}
