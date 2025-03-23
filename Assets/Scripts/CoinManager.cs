0a6af1c0fd4f7c0046860020599da2397fc1f39c
Slighthand <adeline.siu251@ycdsbk12.ca>
2025-03-22 22:02:06 -0400

    changing collectable

--- Assets/Scripts/CoinManager.cs
+++
@@ -1,9 +0,0 @@
-using UnityEngine;
-using UnityEngine.UI;
-public class CoinManager : MonoBehaviour
-{
-    public int CoinCount;
-    public GameObject counterText;
-    //public AudioSource collectSound;
-
-}
