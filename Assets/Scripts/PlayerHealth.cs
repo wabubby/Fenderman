c5de8bd8610378f149be1f99b0efa22c422a0c94
anqiey <144811036+anqiey@users.noreply.github.com>
2025-03-28 10:30:13 -0400

    Altered the terrain using 3D assets

--- Assets/Scripts/PlayerHealth.cs
+++ Assets/Scripts/PlayerHealth.cs
@@ -1,16 +1,19 @@
+
 using UnityEngine;
 
 public class PlayerHealth : MonoBehaviour
 {
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    public int maxHealth = 3;
+    public int currentHealth;
     void Start()
     {
-        
+        currentHealth = maxHealth;
     }
 
-    // Update is called once per frame
-    void Update()
-    {
-        
+    void TakeDamage(int damage) {
+        if (currentHealth > 0) {
+            currentHealth -= damage;
+        }
+        // anim.SetBool("IsDead", true);
     }
-}
+} 
