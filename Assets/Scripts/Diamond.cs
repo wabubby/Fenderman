88084f0bd606d00d3537669c6ace207fdc306024
Slighthand <adeline.siu251@ycdsbk12.ca>
2025-03-27 23:48:52 -0400

    Spawned diamonds around map

--- Assets/Scripts/Diamond.cs
+++ Assets/Scripts/Diamond.cs
@@ -2,20 +2,50 @@ using UnityEngine;
 
 public class Diamond : MonoBehaviour
 {
-
+    [SerializeField]
+    private GameObject diamondPrefab;
     private void OnTriggerEnter(Collider other) 
     {
-        PlayerInventory playerInventroy = other.GetComponent<PlayerInventory>(); // check if collison is with char
+        PlayerInventory playerInventory = other.GetComponent<PlayerInventory>(); // check if collison is with char
 
-        if (playerInventroy != null)
+        if (playerInventory != null)
         {
-            playerInventroy.DiamondCollected();
+            playerInventory.DiamondCollected();
             gameObject.SetActive(false);
+            Spawn();
         }
     }
 
     private void Update()
     {
+        //if (gameObject == false)
+        //{
+        //    Spawn();
+        //}
 
     }
+
+    private void Spawn()
+    {
+        Vector3 randomSpawnPosition = new Vector3(Random.Range(-10, -50), -124, Random.Range(-20, -28));
+        GameObject clone = Instantiate(diamondPrefab, randomSpawnPosition, Quaternion.identity);
+        clone.SetActive(true);
+    }
+
+    //private void DiamondView(int amount)
+    //{
+    //    if (amount == 0)
+    //    {
+    //        diamondView = 0;
+    //    }
+    //}
+
+    //private bool ShouldSpawn()
+    //{
+    //    if (diamondView == 0)
+    //    {
+    //        return true;
+    //    }
+    //    return false;
+    //}
 }
--- Assets/Scripts/PlayerInventory.cs
+++ Assets/Scripts/PlayerInventory.cs
@@ -2,15 +2,17 @@ using UnityEngine;
 using UnityEngine.Events;
 public class PlayerInventory : MonoBehaviour
 {
-    static int scoreGoal = 1; // change to 8
+    static int scoreGoal = 8;
     public int NumberOfDiamonds { get; private set; } // all scripts can read the value,
                                                       // but only this script can set the value
+
     public UnityEvent<PlayerInventory> OnDiamondCollected;
     public void DiamondCollected()
     {
         NumberOfDiamonds++;
         OnDiamondCollected.Invoke(this);
     }
+
     private void Update()
     {
         if (NumberOfDiamonds >= scoreGoal)
