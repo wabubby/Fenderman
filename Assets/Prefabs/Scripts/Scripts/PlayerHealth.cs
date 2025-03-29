200dff5d7ba5bc900ed93c59d9c7c08ab794f44e
anqiey <144811036+anqiey@users.noreply.github.com>
2025-03-28 22:13:28 -0400

    Altered the code for the health bar

--- Assets/Prefabs/Scripts/Scripts/PlayerHealth.cs
+++ Assets/Prefabs/Scripts/Scripts/PlayerHealth.cs
@@ -1,35 +1,58 @@
-
 using System;
 using UnityEngine;
 using UnityEngine.UI;
 using UnityEngine.SceneManagement;
 
-
 public class PlayerHealth : MonoBehaviour
 {
-
-    public int maxHealth = 3;
-    [SerializeField] GameObject[] hearts;
-    public int currentHealth;
-    private void Start()
+    private int currentHealth = 3;
+    
+    public int CurrentHealth 
     {
-        currentHealth = maxHealth;
-    }
-
-    private void OnTriggerEnter(Collider other) {
-        if (other.gameObject.CompareTag("Enemy")) {
-            if (currentHealth >= 1){
-                Destroy(hearts[currentHealth-1]);
-                currentHealth -= 1;
-            }
-            if(currentHealth <= 1){
-                Debug.Log("You died!");
-                SceneManager.LoadScene("LoseScreen");
-                // Switch Scene
-                return;
-            }
-         
+        get { return currentHealth; }
+        private set 
+        {
+            currentHealth = Mathf.Max(0, value); // Ensure health doesn't go below 0
+            UpdateHealthDisplay();
         }
     }
 
-} 
+    [SerializeField] private GameObject[] hearts; 
+
+    // Damage control
+    public void TakeDamage(int damageAmount = 1)
+    {
+        if (CurrentHealth <= 0) return; // Already dead
+        
+        CurrentHealth -= damageAmount; 
+        
+        if (CurrentHealth <= 0)
+        {
+            Die();
+        }
+    }
+
+    // Health Display
+    private void UpdateHealthDisplay()
+    {
+        for (int i = 0; i < hearts.Length; i++)
+        {
+            hearts[i].SetActive(i < CurrentHealth);
+        }
+    }
+
+    // Death
+    private void Die()
+    {
+        Debug.Log("You died!");
+        SceneManager.LoadScene("LoseScreen");
+    }
+
+    private void OnTriggerEnter(Collider other) 
+    {
+        if (other.gameObject.CompareTag("Enemy")) 
+        {
+            TakeDamage();
+        }
+    }
+}
